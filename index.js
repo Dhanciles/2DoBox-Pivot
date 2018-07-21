a17046b639a0cd960488cee2f9dbf4ed889b177c
Drake Bott <bott.da@gmail.com>
2018-07-21 15:16:43 -0600

    Remove anonymous functions

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var qualityVariable = "swill";
-
+//Load cards from localStorage
 for (i = 0; i < localStorage.length; i ++) {
   var key = localStorage.key(i);
   var cardData = JSON.parse(localStorage.getItem(key));
-  console.log(key);
   $(".bottom-box").prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
-  }
+};
 
+//Event listeners
+$('.save-btn').on('click', saveBtn);
+$(".bottom-box").on('click', eventDelegation)
+
+//Functions
 function newCard(id , title , body , quality) {
   return `<div id=${id} class="card-container">
-            <h4 class="title-of-card">${title}<h4>
+            <h4 class="title-of-card">${title}</h4>
             <button class="delete-button"></button>
-            <p class="body-of-card">${body}<p>
+            <p class="body-of-card">${body}</p>
             <button class="upvote"></button>
             <button class="downvote"></button>
             <p class="quality">quality:<span class="qualityVariable">${quality}</span>
@@ -24,35 +27,34 @@ function cardObject() {
   return {
     title: $('.title-input').val(),
     body: $('.body-input').val(),
-    quality: qualityVariable,
+    quality: "swill",
     id: Date.now()
-  };
-}
+  }
+};
 
 function localStoreCard(card) {
   var cardString = JSON.stringify(card);
   localStorage.setItem(card.id , cardString);
-}
+};
 
-$('.save-btn').on('click', function(event) {
+function saveBtn(event) {
   event.preventDefault();
   var cardObj = cardObject();
   var card = newCard(cardObj.id, cardObj.title, cardObj.body, cardObj.quality);
   $( ".bottom-box" ).prepend(card);
   localStoreCard(cardObj);
   $('form')[0].reset();
-});
+};
 
-$(".bottom-box").on('click', function(event){
+function eventDelegation(event){
   var currentQuality = $(event.target).siblings('.quality').text();
   var cardHTML = $(event.target).closest('.card-container');
   var cardID = cardHTML[0].id;
   var cardObj = JSON.parse(localStorage.getItem(cardID));
-  console.log(cardID);
   if (event.target.className === "delete-button") { deleteFunction(event, cardID) }
-});
+};
 
 function deleteFunction(e, id) {
   localStorage.removeItem(id);
   $(e.target).closest('.card-container').remove();
-}
+};
