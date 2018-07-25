cb585200dd253ac53d261ef8bae3e922dbe49c55
Drake Bott <bott.da@gmail.com>
2018-07-24 19:50:08 -0600

    Fix branch

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ $('form').on('keyup', enableSave);
 $('.bottom-box').on('keyup', saveEdit);
 $('.search-input').on('keyup',filterCards);
 $('.filter-buttons').on('click',filterButton);
-$('.');
 
 //Functions
 function newCard(id , title , body , importance) {
